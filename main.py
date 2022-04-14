9ceba89d00568681ff1bcd220882ef58cdf56d5c
Miroslav.Dilov <mirkos354@abv.bg>
2022-04-14 09:10:24 +0300

    Got All Rows in Most Played Games

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+# import requests
 import time
 
 driver = webdriver.Chrome("D:\Chrome Driver\chromedriver.exe")
 
 driver.get("https://steamdb.info/graph/")
 
-# print(driver.find_element(by='tag_name', value='tbody'))
-time.sleep(3)
-print(driver.find_element(by='class', value='app'))
+select_button = driver.find_element(by='name', value='table-apps_length')
+select_button.click()
+
+all_button = driver.find_element(by='xpath', value='//*[@id="table-apps_length"]/label/select/option[7]')
+all_button.click()
+
+
+html = driver.page_source
+
+soup = BeautifulSoup(html, 'html.parser')
+
+rows = soup.find_all('tr', class_='app')
+
+for row in rows:
+    # print(f"Name: {row.contents[5].a.contents[0]}" )
+    name = row.contents[5].a.contents[0]
+    current_players = row.contents[7].contents[0]
+    twenty_four_hour_peak = row.contents[9].contents[0]
+    all_time_peak = row.contents[11].contents[0]
+
+    print(f"Name: {name}")
+    print(f"Current Players: {current_players}")
+    print(f"24 Hour Peak: {twenty_four_hour_peak}")
+    print(f"All Time Peak: {all_time_peak}")
+    print("=======")
+
+driver.quit()
