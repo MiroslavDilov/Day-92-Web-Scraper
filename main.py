0c7f6d72e67985383986d0d9a4d91d5bdcb6f7d1
Miroslav.Dilov <mirkos354@abv.bg>
2022-04-14 09:21:26 +0300

    Exported most played games to csv using Pandas

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 # import requests
 import time
+import pandas as pd
 
 driver = webdriver.Chrome("D:\Chrome Driver\chromedriver.exe")
 
@@ -12,6 +13,9 @@ select_button.click()
 
 all_button = driver.find_element(by='xpath', value='//*[@id="table-apps_length"]/label/select/option[7]')
 all_button.click()
+
+# Creating dataframe
+df = pd.DataFrame(columns=["Name", "Current Players", "24 Hour Peak", "All Time Peak"])
 
 
 html = driver.page_source
@@ -27,10 +31,18 @@ for row in rows:
     twenty_four_hour_peak = row.contents[9].contents[0]
     all_time_peak = row.contents[11].contents[0]
 
-    print(f"Name: {name}")
-    print(f"Current Players: {current_players}")
-    print(f"24 Hour Peak: {twenty_four_hour_peak}")
-    print(f"All Time Peak: {all_time_peak}")
-    print("=======")
+    df = df.append({
+        'Name': name,
+        "Current Players": current_players,
+        "24 Hour Peak": twenty_four_hour_peak,
+        "All Time Peak": all_time_peak
+    }, ignore_index=True)
+    # print(f"Name: {name}")
+    # print(f"Current Players: {current_players}")
+    # print(f"24 Hour Peak: {twenty_four_hour_peak}")
+    # print(f"All Time Peak: {all_time_peak}")
+    # print("=======")
 
+print(df.head(10))
+df.to_csv("most_played_games.csv")
 driver.quit()
